dd022ddfd1bb1436d4e954dbc25b2ec751db482e
codingbbq <codingbbq@gmail.com>
2023-11-14 19:40:37 +0530

    Users and Products page

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,8 +1,74 @@
+import styles from "@/app/ui/dashboard/products/products.module.css";
+import Search from "@/app/ui/dashboard/search/Search";
+import Link from "next/link";
+import Image from "next/image";
+import Pagination from "@/app/ui/dashboard/pagination/Pagination";
+
 const Products = () => {
     return (
         <>
-            Products
-        </>
+			<section className={styles.container}>
+				<div className={styles.top}>
+					<Search placeholder="Search for a product" />
+					<Link href="/dashboard/product/add">
+						<button className={styles.addButton}>Add New</button>
+					</Link>
+				</div>
+				<table className={styles.table}>
+					<thead>
+						<tr>
+							<td>Title</td>
+							<td className={styles.desc}>Description</td>
+							<td>Prices</td>
+							<td>Created At</td>
+							<td>Stock</td>
+							<td>Action</td>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>
+								<div className={styles.product}>
+									<Image
+										src="/noproduct.jpg"
+										alt="Product"
+										width={40}
+										height={40}
+										className={styles.productImage}
+									/>
+									<div className={styles.productName}>
+										IPhone
+									</div>
+								</div>
+							</td>
+							<td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, temporibus?</td>
+							<td>$999</td>
+							<td>13.10.2023</td>
+							<td>72</td>
+							<td>
+								<div className={styles.button}>
+									<Link href="/">
+										<button
+											className={`${styles.button} ${styles.view}`}
+										>
+											View
+										</button>
+									</Link>
+									<Link href="/">
+										<button
+											className={`${styles.button} ${styles.delete}`}
+										>
+											Delete
+										</button>
+									</Link>
+								</div>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+                <Pagination />
+			</section>
+		</>
     )
 };
 
--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,9 +1,75 @@
+import Search from "@/app/ui/dashboard/search/Search";
+import styles from "@/app/ui/dashboard/users/users.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import Pagination from "@/app/ui/dashboard/pagination/Pagination";
+
 const Users = () => {
-    return (
-        <>
-            Users
-        </>
-    )
+	return (
+		<>
+			<section className={styles.container}>
+				<div className={styles.top}>
+					<Search placeholder="Search for a user" />
+					<Link href="/dashboard/users/add">
+						<button className={styles.addButton}>Add New</button>
+					</Link>
+				</div>
+				<table className={styles.table}>
+					<thead>
+						<tr>
+							<td>Name</td>
+							<td>Email</td>
+							<td>Created At</td>
+							<td>Role</td>
+							<td>Status</td>
+							<td>Action</td>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>
+								<div className={styles.user}>
+									<Image
+										src="/noavatar.png"
+										alt="User"
+										width={40}
+										height={40}
+										className={styles.userImage}
+									/>
+									<div className={styles.userName}>
+										John Doe
+									</div>
+								</div>
+							</td>
+							<td>john@gmail.com</td>
+							<td>13.10.2023</td>
+							<td>Admin</td>
+							<td>active</td>
+							<td>
+								<div className={styles.button}>
+									<Link href="/">
+										<button
+											className={`${styles.button} ${styles.view}`}
+										>
+											View
+										</button>
+									</Link>
+									<Link href="/">
+										<button
+											className={`${styles.button} ${styles.delete}`}
+										>
+											Delete
+										</button>
+									</Link>
+								</div>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+                <Pagination />
+			</section>
+		</>
+	);
 };
 
 export default Users;
